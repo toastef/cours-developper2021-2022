739ab7818a5138bb4b961720e5d3285821c43115
toastef <toader.stefan.hmi@gmail.com>
2022-04-07 13:24:18 +0200

    update

--- action/thirdA.php
+++ action/thirdA.php
@@ -65,261 +65,34 @@ if (isset($_POST['submit']) && !empty($_POST['choice1']) && !empty($_POST['choic
 
 
 } elseif (isset($_POST['submit']) && !empty($_POST['choice1']) && !empty($_POST['choice2']) && !empty($_POST['multi'])) {
-    switch ($_POST['choice1']) {
-        case 'firstname':
-            switch ($_POST['choice2']) {
-                case 'de':
-                    ?> Voici les prénom trouvé :
-                    <ul>
-                        <?php
-                        for ($i = 1; $i <= 5; $i++) {
-                            $prenom = $firstname['de'][mt_rand(0, 12)];
-                            ?>
-                            <li><?php
-                                echo $prenom . "<br>";
-                                ?></li>
-
-                        <?php } ?>
-                    </ul>
-                    <?php
-                    break;
-                case 'en':
-                    ?> Voici les prénom trouvé :
-                    <ul>
-                        <?php
-                        for ($i = 1; $i <= 5; $i++) {
-                            $prenom = $firstname['en'][mt_rand(0, 12)];
-                            ?>
-                            <li><?php
-                                echo $prenom . "<br>";
-                                ?></li>
-
-                        <?php } ?>
-                    </ul>
-                    <?php
-                    break;
-                case 'fr':
-
-                    ?> Voici les prénom trouvé :
-                    <ul>
-                        <?php
-                        for ($i = 1; $i <= 5; $i++) {
-                            $prenom = $firstname['fr'][mt_rand(0, 12)];
-                            ?>
-                            <li><?php
-                                echo $prenom . "<br>";
-                                ?></li>
-
-                        <?php } ?>
-                    </ul>
-                    <?php
-                    break;
-                case 'it':
-
-                    ?> Voici les prénom trouvé :
-                    <ul>
-                        <?php
-                        for ($i = 1; $i <= 5; $i++) {
-                            $prenom = $firstname['it'][mt_rand(0, 12)];
-                            ?>
-                            <li><?php
-                                echo $prenom . "<br>";
-                                ?></li>
-
-                        <?php } ?>
-                    </ul>
-                    <?php
-                    break;
+    $tmp = array();
+    // Tant que le nombre d'éléments de notre array n'est pas égal à la quantité demandée, la boucle s'exécute
+   for($i=1;$i<=4;$i++){
+        if($_POST['choice1'] == 'lastname' || $_POST['choice1'] == 'firstname'){
+            if($_POST['choice1'] == 'lastname'){
+                // On insère un élément aléatoire du tableau $firstname dans notre tableau $tmp
+                $tmp[] = $firstname[$_POST['choice2']][array_rand($firstname[$_POST['choice2']])];
+                // A chaqua insertion dans notre tableau, on effectue un tri pour retirer les doublons
+                $tmp = array_unique($tmp);
             }
-
-        case 'lastname':
-            switch ($_POST['choice2']) {
-                case 'de':
-                    ?> Voici les noms trouvés :
-                    <ul>
-                        <?php
-                        for ($i = 1; $i <= 5; $i++) {
-                            $nom = $lastname['de'][mt_rand(0, 12)];
-                            ?>
-                            <li><?php
-                                echo $nom . "<br>";
-                                ?></li>
-
-                        <?php } ?>
-                    </ul>
-                    <?php
-                    break;
-                case 'en':
-                    ?> Voici les noms trouvés :
-                    <ul>
-                        <?php
-                        for ($i = 1; $i <= 5; $i++) {
-                            $nom = $lastname['en'][mt_rand(0, 12)];
-                            ?>
-                            <li><?php
-                                echo $nom . "<br>";
-                                ?></li>
-
-                        <?php } ?>
-                    </ul>
-                    <?php
-                    break;
-                case 'fr':
-
-                    ?> Voici les noms trouvés :
-                    <ul>
-                        <?php
-                        for ($i = 1; $i <= 5; $i++) {
-                            $nom = $lastname['fr'][mt_rand(0, 12)];
-                            ?>
-                            <li><?php
-                                echo $nom . "<br>";
-                                ?></li>
-
-                        <?php } ?>
-                    </ul>
-                    <?php
-                    break;
-                case 'it':
-
-                    ?> Voici les noms trouvés :
-                    <ul>
-                        <?php
-                        for ($i = 1; $i <= 5; $i++) {
-                            $nom = $lastname['it'][mt_rand(0, 12)];
-                            ?>
-                            <li><?php
-                                echo $nom . "<br>";
-                                ?></li>
-
-                        <?php } ?>
-                    </ul>
-                    <?php
-                    break;
+            elseif($_POST['choice1'] == 'firstname'){
+                $tmp[] = $lastname[$_POST['choice2']][array_rand($lastname[$_POST['choice2']])];
+                $tmp = array_unique($tmp);
             }
-        case 'both' :
-            switch ($_POST['choice2']) {
-                case 'de':
-                    ?> Voici les prénom trouvé :
-                    <ul>
-                        <?php
-                        for ($i = 1; $i <= 5; $i++) {
-                            $prenom = $firstname['de'][mt_rand(0, 12)];
-                            ?>
-                            <li><?php
-                                echo $prenom . "<br>";
-                                ?></li>
-
-                        <?php } ?>
-                    </ul>
-                    <?php
-
-                    echo "<br>";
-                    ?> Voici les noms trouvés :
-                    <ul>
-                        <?php
-                        for ($i = 1; $i <= 5; $i++) {
-                            $nom = $lastname['de'][mt_rand(0, 12)];
-                            ?>
-                            <li><?php
-                                echo $nom . "<br>";
-                                ?></li>
-
-                        <?php } ?>
-                    </ul>
-                    <?php
-                    break;
-                case 'en':
-                    ?> Voici les prénom trouvé :
-                    <ul>
-                        <?php
-                        for ($i = 1; $i <= 5; $i++) {
-                            $prenom = $firstname['en'][mt_rand(0, 12)];
-                            ?>
-                            <li><?php
-                                echo $prenom . "<br>";
-                                ?></li>
-
-                        <?php } ?>
-                    </ul>
-                    <?php
-
-                    echo "<br>";
-                    ?> Voici les noms trouvés :
-                    <ul>
-                        <?php
-                        for ($i = 1; $i <= 5; $i++) {
-                            $nom = $lastname['en'][mt_rand(0, 12)];
-                            ?>
-                            <li><?php
-                                echo $nom . "<br>";
-                                ?></li>
-
-                        <?php } ?>
-                    </ul>
-                    <?php
-                    break;
-                case 'fr':
-                    ?> Voici les prénom trouvé :
-                    <ul>
-                        <?php
-                        for ($i = 1; $i <= 5; $i++) {
-                            $prenom = $firstname['fr'][mt_rand(0, 12)];
-                            ?>
-                            <li><?php
-                                echo $prenom . "<br>";
-                                ?></li>
-
-                        <?php } ?>
-                    </ul>
-                    <?php
-                    echo "<br>";
-                    ?> Voici les noms trouvés :
-                    <ul>
-                        <?php
-                        for ($i = 1; $i <= 5; $i++) {
-                            $nom = $lastname['fr'][mt_rand(0, 12)];
-                            ?>
-                            <li><?php
-                                echo $nom . "<br>";
-                                ?></li>
-
-                        <?php } ?>
-                    </ul>
-                    <?php
-                    break;
-                case 'it':
-                    ?> Voici les prénom trouvé :
-                    <ul>
-                        <?php
-                        for ($i = 1; $i <= 5; $i++) {
-                            $prenom = $firstname['it'][mt_rand(0, 12)];
-                            ?>
-                            <li><?php
-                                echo $prenom . "<br>";
-                                ?></li>
-
-                        <?php } ?>
-                    </ul>
-                    <?php
-                    echo "<br>";
-                    ?> Voici les noms trouvés :
-                    <ul>
-                        <?php
-                        for ($i = 1; $i <= 5; $i++) {
-                            $nom =$lastname['it'][mt_rand(0, 12)];
-                            ?>
-                            <li><?php
-
-                                    echo $nom . "<br>";
-                                ?></li>
-
-                        <?php } ?>
-                    </ul>
-                    <?php
-                    break;
-            }
+        } elseif($_POST['choice1'] == 'both'){
+            $first_name = $firstname[$_POST['choice2']][array_rand($firstname[$_POST['choice2']])];
+            $last_name = $lastname[$_POST['choice2']][array_rand($lastname[$_POST['choice2']])];
+            // On effectue la même opération qu'avant, mais on concatène les deux données pour le nom + prénom
+            $tmp[] = $first_name .' '. $last_name;
+            $tmp = array_unique($tmp);
+        }
     }
-}
+    // Une fois toutes les données nécéssaires insérées dans notre tableau $tmp, on print.
 
+    foreach($tmp as $key => $value){
+        ?> <ul><li><?php echo $value.'<br>'?></li></ul>  <?php
+    }
+} else {
+    header('Location: ./index.php');
+}
 
